dcbaefa5d1070685d56b88cca869bf6c47749155
HYEMIN <hy1x1mn@gmail.com>
2024-04-29 22:19:41 +0900

    READ : Products 상품 목록 조회

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductListAPIView(APIView) :
+    def get(self,request) :
+        products = Product.objects.all()
+        serializers = ProductSerializer(products,many=True)
+        json_response = serializers.data
+        return Response(json_response)
